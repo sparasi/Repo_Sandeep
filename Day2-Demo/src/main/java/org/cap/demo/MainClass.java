f6c33ea1f4f18283402cde8f95fb7ce7ed1cd9c9
lnc <lnc@DIN76000159.corp.capgemini.com>
2017-08-09 19:34:32 +0530

    commit the changes in branch2

--- Day2-Demo/src/main/java/org/cap/demo/MainClass.java
+++ Day2-Demo/src/main/java/org/cap/demo/MainClass.java
@@ -3,7 +3,7 @@ package org.cap.demo;
 public class MainClass {
 
 	public static void greetings(){
-		System.out.println("Hey! Good AfterNoon!");
+		System.out.println("Hey Super! Good AfterNoon!");
 	}
 	
 	public static void main(String[] args) {
